4c937bf363b37a09392138851733da8396bcfe9c
declan015 <henriquegou310822@gmail.com>
2020-05-15 16:59:41 -0300

    Atualização de diretorios

--- Codes/Aula2/var.py
+++
@@ -1,2 +0,0 @@
-variavel = "Hello World"
-print(variavel)
--- Codes/Aula2/var2.py
+++
@@ -1,6 +0,0 @@
-variavel = ("hello worLD")
-
-print("Original: " + variavel)
-print("Com title(): " + variavel.title())
-print("Com upper(): " + variavel.upper())
-print("Com lower(): " + variavel.lower())
